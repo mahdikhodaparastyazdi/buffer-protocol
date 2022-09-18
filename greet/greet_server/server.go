aca5d60acfbb5de80e51d2f6b77cfc4c6c04bccd
unknown <mahdi.kho59@gmail.com>
2022-09-18 16:12:28 +0430

    problem with internal package

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,12 +1,16 @@
-package main
+package greetserver
 
 import (
 	"fmt"
+
+	"grpc_course/greet/greetpb"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 )
+
+type server struct{}
 
 func main() {
 
@@ -17,4 +21,9 @@ func main() {
 		log.Fatalf("Failed to listion: %v", err)
 	}
 	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed: %v", err)
+	}
 }
---
+++ greet/greetpb/go.mod
@@ -0,0 +1,5 @@
+module grpc_course/greet/greetpb
+
+go 1.19
+
+require google.golang.org/protobuf v1.28.1
